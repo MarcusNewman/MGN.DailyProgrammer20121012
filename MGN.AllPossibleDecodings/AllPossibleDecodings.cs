779d544179d2717015051f3a14186c3217746dad
Marcus Newman <newmanm@sti.net>
2012-10-27 00:54:03 -0700

    in progress checkin

--- MGN.AllPossibleDecodings/AllPossibleDecodings.cs
+++ MGN.AllPossibleDecodings/AllPossibleDecodings.cs
@@ -2,11 +2,28 @@
 
 namespace MGN.AllPossibleDecodings
 {
+    //Consider the translation from letters to numbers a -> 1 through z -> 26. Every sequence of letters can be translated into a string of numbers this way,
+    //with the numbers being mushed together. For instance hello -> 85121215. Unfortunately the reverse translation is not unique. 85121215 could map to hello,
+    //but also to heaubo. Write a program that, given a string of digits, outputs every possible translation back to letters.
+    //Sample input:
+    //123
+    //Sample output:
+    //abc
+    //aw
+    //lc
     public class AllPossibleDecodings
     {
         public static string[] GetAllPossibleDecodings(String input)
         {
-            return new string[] { "abc", "aw", "lc" };
+            var result = string.Empty;
+            //lets get it to return abc first
+            foreach(var letter in input.ToCharArray())
+            {
+                var value = Convert.ToInt32(letter);
+                result += (char)(value + 97-49);
+            }
+            return new string[] { result };
+            //return new string[] { "abc", "aw", "lc" };
             //return new string[] { "hello" };
         }
     }
